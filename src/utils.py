5cce67032b4193907e1747f23326c822f64fc01c
Kuricheti Prerana <preranakuricheti18@gmail.com>
2026-07-03 11:34:23 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import json
 import numpy as np
 import pandas as pd
 # csv file reading
-df=pd.read_csv("data\xy_data.csv")
+df=pd.read_csv("data/xy_data.csv")
 
 def data_load():
      # convert the x and y columns into a numpy array
